bb1552c29ceb17274273efe4338d3ed68d099fb0
hanif2023 <hanifabdillah150703@gmail.com>
2023-10-22 23:08:24 +0700

    this is my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import Select from 'react-select'
 
 function App() {
+  const [datas, setDatas] = useState([])
+  const [userSelect, setUserSelect] = useState("")
+  const [isShow, setIsShow] = useState(false)
+
+  const getBerries = async() => {
+    const berries = await fetch("https://pokeapi.co/api/v2/berry/")
+    const value = await berries.json()
+    let result = value.results.map(data => {
+      return {
+        label: data.name,
+        value: data.name
+      }
+    })
+    setDatas(result.sort((a, b) => a.label.localeCompare(b.label)))
+  }
+  
+  useEffect(()=> {
+    getBerries()
+  }, [])
+
+  const handleSubmit = () => {
+    setIsShow(state => !state)
+  }
+
+  const handleChange = (value) => {
+    setUserSelect(value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Select className='px-10 pt-10 py-5 capitalize' options={datas} onChange={(e) => handleChange(e.value)}></Select>
+      <button className='w-40 bg-black p-2 rounded-lg text-white' onClick={() => handleSubmit()} disabled={!userSelect}>{isShow ? "Hide Values" : "Show Values"}</button>
+      <br/>
+      <br/>
+      <h1 className='text-4xl font-bold underline capitalize'> {isShow ? userSelect: ""} </h1>
     </div>
   );
 }
